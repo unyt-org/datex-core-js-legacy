c63f411bf05af9f2a9122916da8a546440791203
benStre <strehle.benedikt@gmail.com>
2023-11-30 20:24:46 +0100

    add error reporting

---
+++ utils/error-reporting.ts
@@ -0,0 +1,37 @@
+import { Compiler } from "../compiler/compiler.ts";
+import { sendDatexViaHTTPChannel } from "../network/datex-http-channel.ts";
+import { Runtime } from "../runtime/runtime.ts";
+import { getCallerInfo } from "../utils/caller_metadata.ts";
+import { logger } from "./global_values.ts";
+
+export async function sendReport(id: string, reportData:Record<string,any>) {
+	if (!enabled) return;
+
+	const report = {
+		id,
+		timestamp: new Date(),
+		metaData: {
+			datexcoreVersion: Runtime.VERSION,
+			uixVersion: globalThis.UIX?.version,
+			denoVersion: globalThis.Deno?.version.deno,
+			tsVersion: globalThis.Deno?.version.typescript,
+			v8Version: globalThis.Deno?.version.v8,
+			userAgent: JSON.parse(JSON.stringify(window.navigator.userAgentData ?? window.navigator.userAgent))
+		},
+		reportData,
+		stack: getCallerInfo()
+	}
+
+	console.log("report", report);
+
+	const dx = `@+unyt_status.Reporting.sendReport(?)`
+	const dxb = <ArrayBuffer> await Compiler.compile(dx, [report], {sign: false, encrypt: false});
+	sendDatexViaHTTPChannel(dxb, "https://status.unyt.org")
+}
+
+let enabled = false;
+
+export function enableErrorReporting(enable = true) {
+	logger.debug("error reporting to unyt.org " + (enable ? "enabled" : "disabled"));
+	enabled = enable;
+}
