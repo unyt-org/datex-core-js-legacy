65185fb5b0dbe35b0199c309db75b6f3751424b4
benStre <strehle.benedikt@gmail.com>
2024-03-26 22:50:37 +0100

    refactor command-line-options

--- utils/args.ts
+++ utils/args.ts
@@ -1,12 +1,4 @@
-// TODO: fix this (cdn problems)
-let CommandLineOptions: any // typeof import("https://dev.cdn.unyt.org/command-line-args/main.ts").CommandLineOptions;
-try {
-    ({CommandLineOptions} = await import("https://dev.cdn.unyt.org/"+"command-line-args/main.ts"));
-}
-catch {
-    console.warn("using fallback CommandLineOptions module, cdn not reachable");
-    ({CommandLineOptions} = <any> await import("./_command_line_options.ts"));
-}
+import {CommandLineOptions} from "./command-line-args/main.ts";
 
 export const commandLineOptions = new CommandLineOptions("DATEX Core", "DATEX Runtime for JavaScript/TypeScript.\nVisit https://unyt.org/datex for more information");
 export const clear = commandLineOptions.option("clear", {type:"boolean", description: "Clear all eternal states on the backend"});
