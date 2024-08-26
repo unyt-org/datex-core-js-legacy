984824261ece14f66ed279cd328a19d451ae3b2a
benStre <jonas@strehles.info>
2024-08-26 21:48:33 +0200

    Add pipe

--- network/debugging/collector-target.ts
+++ network/debugging/collector-target.ts
@@ -8,9 +8,15 @@ import { DebuggingInterface } from "./debugging-interface.ts";
 export class CollectorDebuggingInterface {
 	private static debuggingInterfaces: Map<DebuggingInterface, Stream<MessageStream> | undefined> = new Map();
 	private static instance: CollectorDebuggingInterface | undefined
+	private static collectorStreams: Set<Stream<MessageStream>> = new Set();
 
 	@property public static async registerInterface(interf: DebuggingInterface) {
 		this.debuggingInterfaces.set(interf, undefined);
+		
+		// only call the getMessage for a stream when at least one collector stream is active
+		// aka getMessages was not called before
+		if (this.collectorStreams.size)
+			await this.pipeToStreams(interf, this.collectorStreams);
 	}
 
 	@property public static get() {
@@ -19,14 +25,20 @@ export class CollectorDebuggingInterface {
 		return this.instance;
 	}
 
+	private static async pipeToStreams(interf: DebuggingInterface, streams: Iterable<Stream<MessageStream>>) {
+		const interfaces = CollectorDebuggingInterface.debuggingInterfaces;
+		if (interfaces.get(interf) == undefined)
+			interfaces.set(interf, await interf.getMessages());
+		for (const stream of streams)
+			interfaces.get(interf)!.pipeTo(stream.writable_stream)
+	}
+
 	@property async getMessages(filter: MessageFilter = { }) {
 		const list = CollectorDebuggingInterface.debuggingInterfaces;
 		const collectorStream = new Stream<MessageStream>();
-		for (const [interf, stream] of list) {
-			if (stream == undefined)
-				list.set(interf, await interf.getMessages());
-			list.get(interf)!.pipeTo(collectorStream.writable_stream);
-		}
+		CollectorDebuggingInterface.collectorStreams.add(collectorStream);
+		for (const [interf] of list)
+			CollectorDebuggingInterface.pipeToStreams(interf, [collectorStream]);
 		return collectorStream;
 	}
 }
