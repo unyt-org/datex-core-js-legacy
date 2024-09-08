ea8cc2aef292d89fe61bf1e4de4259abdec977ce
benStre <jonas@strehles.info>
2024-09-08 20:00:18 +0200

    fixes for deno 2.0

--- utils/sha256.ts
+++ utils/sha256.ts
@@ -16,7 +16,7 @@ if (root.JS_SHA256_NO_WINDOW) {
     WINDOW = false;
 }
 var WEB_WORKER = !WINDOW && typeof self === 'object';
-var NODE_JS = !root.JS_SHA256_NO_NODE_JS && typeof process === 'object' && process.versions && process.versions.node;
+var NODE_JS = !globalThis.Deno && !root.JS_SHA256_NO_NODE_JS && typeof process === 'object' && process.versions && process.versions.node;
 if (NODE_JS) {
     root = global;
 } else if (WEB_WORKER) {
