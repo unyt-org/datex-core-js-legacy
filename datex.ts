a841ab16d1eada9f3fa995373d2701f198da04cf
benStre <jonas@strehles.info>
2024-10-11 11:45:48 +0200

    fix globalThis.window

--- datex.ts
+++ datex.ts
@@ -12,7 +12,10 @@ import { Blockchain } from "./network/blockchain_adapter.ts"
 import { Runtime } from "./runtime/runtime.ts";
 Runtime.Blockchain = Blockchain;
 
+
 // workaround for backwards compatibility with Deno < 2.0.0
-globalThis.window = globalThis;
+if (!globalThis.window) {
+	globalThis.window = globalThis;
+}
 
 await init();
