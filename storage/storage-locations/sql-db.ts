f0f3ad221b9f689166983ac5986c41eff485b12e
benStre <jonas@strehles.info>
2025-08-22 22:07:22 +0200

    fix multiple group bys

--- storage/storage-locations/sql-db.ts
+++ storage/storage-locations/sql-db.ts
@@ -1184,10 +1184,12 @@ export class SQLDBStorageLocation extends AsyncStorageLocation {
 							const values = [...condition.data]
 
 							// NOTE: currently only works for value_text!
-							if (or.type == MatchConditionType.CONTAINS_NONE) {								
+							if (or.type == MatchConditionType.CONTAINS_NONE) {
+								const groupBy = `GROUP BY ${this.#typeToTableName(valueType)}.${this.#pointerMysqlColumnName}`;						
+								const alreadyHasGroupBy = appendStatements.includes(groupBy);
+								if (!alreadyHasGroupBy) appendStatements.push(groupBy);
 								appendStatements.push(
-									replaceParams(`GROUP BY ${this.#typeToTableName(valueType)}.${this.#pointerMysqlColumnName}
-										 HAVING SUM(CASE WHEN ${namespacedKey}.value_text IN (?) THEN 1 ELSE 0 END) = 0`, [
+									replaceParams(`${alreadyHasGroupBy ? 'AND' : 'HAVING'} SUM(CASE WHEN ${namespacedKey}.value_text IN (?) THEN 1 ELSE 0 END) = 0`, [
 										values.map(v => v instanceof Date ? v.toISOString() : v)
 									])
 								)
@@ -1238,12 +1240,16 @@ export class SQLDBStorageLocation extends AsyncStorageLocation {
 
 								// make sure all values exist in the set
 								if (or.type == MatchConditionType.CONTAINS_ALL) {
+									const groupBy = `GROUP BY ${this.#typeToTableName(valueType)}.${this.#pointerMysqlColumnName}`;
+									const alreadyHasGroupBy = appendStatements.includes(groupBy);
+									if (!alreadyHasGroupBy) appendStatements.push(groupBy);
+
 									appendStatements.push(
-										replaceParams(`GROUP BY ${this.#typeToTableName(valueType)}.${this.#pointerMysqlColumnName} HAVING COUNT(*) = ?`, [
+										replaceParams(`${alreadyHasGroupBy ? 'AND' : 'HAVING'} COUNT(*) = ?`, [
 											values.length
 										])
-									)
-								}	
+									);
+								}
 							}
 						}
 
